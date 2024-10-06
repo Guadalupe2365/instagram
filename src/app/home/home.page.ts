ec2b0fff6775b7a0fd880579eb123a5aef4ef2ed
Guadalupe <20223l001004@utcv.edu.mx>
2024-10-05 18:26:07 -0600

    primer commit

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { IonContent, IonHeader,IonAvatar, IonTitle, IonToolbar, IonLabel, IonButton, IonImg, IonBadge, IonGrid, IonRow, IonCol, IonIcon, IonItem, IonText, IonCard } from '@ionic/angular/standalone';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+  standalone: true,
+  imports: [IonCard, IonText, IonItem, IonIcon, IonCol,IonAvatar, IonRow, IonGrid, IonAvatar, IonBadge, IonImg, IonButton, IonLabel, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+})
+export class HomePage {
+  constructor() {}
+}
